ec5a75793a3ed7c0dd520dd1c111b54e6e1e5e9f
turik777 <52492191+turik777@users.noreply.github.com>
2024-11-04 01:28:11 +0300

    feat: complete isPowerOfTwo function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -299,8 +299,8 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return Number.isInteger(Math.log2(num));
 }
 
 /**
